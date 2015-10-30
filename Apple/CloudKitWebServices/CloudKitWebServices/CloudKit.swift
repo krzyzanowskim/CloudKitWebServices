bf132d842d8e4eab84a0cbf7e11750d1947fb3fe
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-10-30 01:18:15 +0100

    Xcode project initials

---
+++ Apple/CloudKitWebServices/CloudKitWebServices/CloudKit.swift
@@ -0,0 +1,14 @@
+//
+//  CloudKit.swift
+//  CloudKitWebServices
+//
+//  Created by Marcin Krzyzanowski on 30/10/15.
+//  Copyright © 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class CloudKit {
+
+}
---
+++ Apple/CloudKitWebServices/CloudKitWebServices/CloudKitWebServices.h
@@ -0,0 +1,19 @@
+//
+//  CloudKitWebServices.h
+//  CloudKitWebServices
+//
+//  Created by Marcin Krzyzanowski on 30/10/15.
+//  Copyright © 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CloudKitWebServices.
+FOUNDATION_EXPORT double CloudKitWebServicesVersionNumber;
+
+//! Project version string for CloudKitWebServices.
+FOUNDATION_EXPORT const unsigned char CloudKitWebServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudKitWebServices/PublicHeader.h>
+
+
